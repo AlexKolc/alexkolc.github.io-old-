5a2c2f2e9066eba43209fd5599de532dfab84a3e
AlexKolc <alex_kolc@inbox.ru>
2020-10-15 17:50:41 +0300

    Add choose weather icon Todo: fix determination of precipitation

--- weather.js
+++ weather.js
@@ -13,7 +13,7 @@ addNewCityForm.addEventListener('submit', (event) => {
 
 function request(params) {
     params.push('units=metric');
-    params.push('appid=f80f663722c0d3dd6beacd446c31524a');
+    params.push('appid=52f8f9af79e0664f928042deb0e2b888');
     const url = 'https://api.openweathermap.org/data/2.5/weather?' + params.join('&');
     const abortController = new AbortController();
     const abortSignal = abortController.signal;
@@ -65,7 +65,7 @@ function fillCurrentCityInfo(params) {
             <div class="current-city">
                 <h2 class="current-city-name">${jsonResult.name}</h2>
                 <div class="current-weather">
-                    <img src="images/broken_clouds.png" class="current-weather-img" alt="weather"/>
+                    <img src="images/weather/${getWeatherIcon(jsonResult)}.png" class="current-weather-img" alt="weather"/>
                     <p class="current-degrees">${Math.floor(jsonResult.main.temp)}&deg;C</p>
                 </div>
             </div>
@@ -212,11 +212,10 @@ function newCityLoaderInfo() {
 function addCity(jsonResult, newCity) {
     const cityName = jsonResult.name;
     newCity.id = cityName.split(' ').join('-');
-    //const imageName = getIcon(jsonResult);
     newCity.innerHTML = `<div class="favorite-weather">
                             <h3>${cityName}</h3>
                             <p class="degrees">${Math.floor(jsonResult.main.temp)}&deg;C</p>
-                            <img src="images/broken_clouds.png" class="favorite-weather-img" alt="weather small"/>
+                            <img src="images/weather/${getWeatherIcon(jsonResult)}.png" class="favorite-weather-img" alt="weather small"/>
                             <button onclick="deleteCity(\'${cityName}\');" class="delete-btn">+</button>
                         </div>
  
@@ -228,6 +227,109 @@ function addCity(jsonResult, newCity) {
 function deleteCity(cityName) {
     localStorage.removeItem(cityName);
     document.getElementById(cityName.split(' ').join('-')).remove();
+}
+
+function getWeatherIcon(jsonResult) {
+    let clouds = haveClouds(jsonResult.clouds.all);
+    let wind = haveWind(jsonResult.wind.speed);
+    let precipitation = havePrecipitation(jsonResult);
+    let timeOfDay = getTimeOfDay(jsonResult);
+
+    if (clouds === 'cloudy' && precipitation === 'no' && wind === 'no') {
+        return 'cloud';
+    } else if (clouds === 'variable' && precipitation === 'no' && wind === 'no' && timeOfDay === 'day') {
+        return 'variable-cloudy-day';
+    } else if (clouds === 'variable' && precipitation === 'no' && wind === 'no' && timeOfDay === 'night') {
+        return 'variable-cloudy-night';
+    } else if (clouds === 'cloudy' && precipitation === 'no' && wind !== 'no') {
+        return 'wind';
+    } else if (clouds === 'variable' && precipitation === 'no' && wind !== 'no' && timeOfDay === 'day') {
+        return 'wind-day';
+    } else if (clouds === 'variable' && precipitation === 'no' && wind !== 'no' && timeOfDay === 'night') {
+        return 'wind-night';
+    } else if (clouds === 'cloudy' && (precipitation === 'rain' || precipitation === 'downpour') && wind === 'tempest') {
+        return 'tempest';
+    } else if (clouds === 'cloudy' && (precipitation === 'rain' || precipitation === 'downpour') && wind === 'tempest' && timeOfDay === 'day') {
+        return 'tempest-day';
+    } else if (clouds === 'cloudy' && (precipitation === 'rain' || precipitation === 'downpour') && wind === 'tempest' && timeOfDay === 'night') {
+        return 'tempest-night';
+    } else if (clouds === 'cloudy' && precipitation === 'mistyrain') {
+        return 'mistyrain';
+    } else if (clouds === 'variable' && precipitation === 'mistyrain' && timeOfDay === 'day') {
+        return 'mistyrain-day';
+    } else if (clouds === 'variable' && precipitation === 'mistyrain' && timeOfDay === 'night') {
+        return 'mistyrain-night';
+    } else if (clouds === 'cloudy' && precipitation === 'rain') {
+        return 'rain';
+    } else if (clouds === 'variable' && precipitation === 'rain' && timeOfDay === 'day') {
+        return 'rain-day';
+    } else if (clouds === 'variable' && precipitation === 'rain' && timeOfDay === 'night') {
+        return 'rain-night';
+    } else if (clouds === 'cloudy' && precipitation === 'downpour') {
+        return 'downpour';
+    } else if (clouds === 'variable' && precipitation === 'downpour' && timeOfDay === 'day') {
+        return 'downpour-day';
+    } else if (clouds === 'variable' && precipitation === 'downpour' && timeOfDay === 'night') {
+        return 'downpour-night';
+    } else if (clouds === 'cloudy' && precipitation === 'snow') {
+        return 'snow';
+    } else if (clouds === 'variable' && precipitation === 'snow' && timeOfDay === 'day') {
+        return 'snow-day';
+    } else if (clouds === 'variable' && precipitation === 'snow' && timeOfDay === 'night') {
+        return 'snow-night';
+    }
+
+    if (timeOfDay === 'night') {
+        return 'moon';
+    }
+    return 'sun';
+}
+
+function haveClouds(clouds) {
+    if (clouds <= 30) { // ясно
+        return 'no';
+    } else if (clouds <= 70) { // переменно
+        return 'variable';
+    }
+    return 'cloudy';
+}
+
+function haveWind(wind) {
+    if (wind < 14) {
+        return 'no';
+    } else if (wind < 33) {
+        return 'windy';
+    }
+    return 'tempest';
+}
+
+function havePrecipitation(jsonResult) {
+    let snow = jsonResult.snow;
+    let rain = jsonResult.rain;
+    if (snow > rain) {
+        if (snow > 0.1) {
+            return 'snow';
+        }
+    } else if (rain >= snow) {
+        if (rain < 3) {
+            return 'mistyrain';
+        } else if (rain < 15) {
+            return 'rain';
+        } else if (rain > 14) {
+            return 'downpour';
+        }
+    }
+    return 'no';
+}
+
+function getTimeOfDay(jsonResult) {
+    let now = jsonResult.dt;
+    let sunrise = jsonResult.sys.sunrise;
+    let sunset = jsonResult.sys.sunset;
+    if (now > sunset) {
+        return 'night';
+    }
+    return 'day';
 }
 
 getLocation();
